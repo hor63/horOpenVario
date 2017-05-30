5711377f3091b20a1977705150cb2eec4ce3fa4c
Kai Horstmann <horstmannkai@hotmail.com>
2017-05-30 21:56:25 +0200

    Build entire installer image from scratch

--- makenewimage.sh
+++ makenewimage.sh
@@ -7,6 +7,25 @@
 
 # set -x
 
+
+( cd src/sunxi-tools
+  echo "rebuild fex compiler"
+  make fexc bin2fex fex2bin || exit 1
+) || exit 1  
+
+echo "Hit enter to continue"
+read x
+
+
+( cd src/uboot-mainline
+  echo "rebuild uboot"
+  make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-  || exit 1
+) || exit 1  
+
+echo "Hit enter to continue"
+read x
+
+
 ( cd src/kernel
   echo "rebuild the kernel"
   make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- uImage modules || exit 1
@@ -19,7 +38,7 @@ read x
 
 ( cd src/rtl8188C_8192C_usb_linux_v4.0.2_9000.20130911
   echo "Build the RTL 8192CU driver"
-  make -i clean
+  # make -i clean
   make || exit 1
 ) || exit 
 
@@ -49,7 +68,7 @@ read x
  then
    echo "Installer is already here. Do you want to download again? [yN]"
    read x
-   if [ "$x" -eq "Y" -o "$x" -eq "y" ]
+   if [ "$x" == "Y" -o "$x" == "y" ]
    then
      rm initrd.gz
    fi
@@ -60,9 +79,9 @@ read x
    echo "initrd.gz is retained"
  else
    echo "Download initrd.gz"
-   wget "http://ports.ubuntu.com/ubuntu-ports/dists/xenial/main/installer-armhf/current/images/generic/netboot/initrd.gz"
+   wget "http://ports.ubuntu.com/ubuntu-ports/dists/xenial/main/installer-armhf/current/images/generic/netboot/initrd.gz" || exit 1
  fi
-)
+) || exit 1
 
 echo "Hit enter to continue"
 read x
@@ -72,8 +91,8 @@ read x
  sudo rm -rf initrd.dir
  mkdir initrd.dir
  cd initrd.dir
- gunzip < ../initrd.gz |sudo cpio -idmu
-)
+ gunzip < ../initrd.gz |sudo cpio -idmu || exit 1
+) || exit 1
 
 echo "Hit enter to continue"
 read x
@@ -84,41 +103,67 @@ read x
  sudo cp -R ../../../../../build/root/lib/modules/3.4* .
  cd ../..
  echo "re-build the initrds"
- find * |cpio -o -H newc |gzip > ../myinitrd.gz
- find dev lib/modules/3.4.* |cpio -o -H newc |gzip > ../initrd.noinst.gz
+ find * |cpio -o -H newc |gzip > ../myinitrd.gz || exit 1
+ find dev lib/modules/3.4.* |cpio -o -H newc |gzip > ../initrd.noinst.gz || exit 1
  cd ..
- mkimage -A arm -T ramdisk -C gzip -d myinitrd.gz uMyinitrd
- mkimage -A arm -T ramdisk -C gzip -d initrd.noinst.gz uInitrdNoinst
- cp -v uMyinitrd  ../boot
- cp -v uInitrdNoinst  ../boot
-)
+ mkimage -A arm -T ramdisk -C gzip -d myinitrd.gz uMyinitrd || exit 1
+ mkimage -A arm -T ramdisk -C gzip -d initrd.noinst.gz uInitrdNoinst || exit 1
+ cp -v uMyinitrd  ../boot || exit 1
+ cp -v uInitrdNoinst  ../boot || exit 1
+) || exit 1
 
 
 echo "make boot script image"  
 ( cd build/boot ; mkimage -A arm -T script -C none -d boot.cmd boot.scr 
-  mkimage -A arm -T script -C none -d boot.noinitrd.cmd boot.noinitrd.scr 
+  mkimage -A arm -T script -C none -d boot.noinitrd.cmd boot.noinitrd.scr  || exit 1
   for i in *.cmd
   do
     s=`basename $i .cmd`.scr
     echo "Make boot script $s from $i"
-    mkimage -A arm -T script -C none -d $i $s
+    mkimage -A arm -T script -C none -d $i $s || exit 1
   done  )
 echo "compile FEX file to script.bin"
-( cd build/boot ; ../../src/sunxi-tools/fex2bin -v openvario.fex script.bin )
+( 
+  cd build/boot ; ../../src/sunxi-tools/fex2bin -v openvario.fex script.bin  || exit 1
+) || exit 1
 
+
+echo "Hit enter to continue"
+read x
+echo "Create and partition the SD image"
+dd if=/dev/zero of=sd.img bs=1M count=110 || exit 1
+echo "o
+n
+p
+1
+2048
++100M
+p
+w
+q" | fdisk sd.img || exit 1
+
+echo "Hit enter to continue"
+read x
 echo "Format and mount the SD image"  
-sudo losetup /dev/loop0 sd.img
-sudo partprobe /dev/loop0
-sudo mkfs.ext2 -F /dev/loop0p1
-sudo mount /dev/loop0p1 sdcard/boot
+sudo losetup /dev/loop0 sd.img || exit 1
+sudo partprobe /dev/loop0 || exit 1
+sudo mkfs.ext2 -F /dev/loop0p1 || exit 1
+sudo mount /dev/loop0p1 sdcard/boot || exit 1
 echo "Copy boot environment ot SD card image"  
-sudo cp -v build/boot/* sdcard/boot
+sudo cp -v build/boot/* sdcard/boot || exit 1
 df
+
+echo "Hit enter to continue"
+read x
+
+echo "Copy U-Boot to the SD image"
+sudo dd if=src/uboot-mainline/u-boot-sunxi-with-spl.bin of=/dev/loop0 bs=1024 seek=8 || exit 1
+
 echo "Unmount the SD card image"  
 sudo umount sdcard/boot
 sudo losetup -D
 sudo losetup 
-# echo "Copy the SD card image to the host"  
+echo "Copy the SD card image \"sd.img\" to the SD card raw device"  
 # cp -v sd.img /mnt/hgfs/D/Users/kai_horstmann/Downloads/Cubieboard/
 echo " ----------------- Done -------------------------"
 
