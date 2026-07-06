844ce003e117d62093944e5ff76297f5222ad28f
Kai Horstmann <horstmannkai@hotmail.com>
2026-07-07 01:20:38 +0200

    More options to improve Qemu emulation performance.

--- startQemuDirect.sh
+++ startQemuDirect.sh
@@ -2,8 +2,10 @@
 
 qemu-system-x86_64 -kernel build/kernel/arch/x86/boot/bzImage -append "console=tty0 root=/dev/sda2 rootwait ro" \
 	-drive file=sd.img,format=raw,if=ide \
-	-vga virtio -display gtk,zoom-to-fit=on,gl=on \
-	-m 2G
+	-vga virtio -display gtk,gl=on \
+	-m 2G \
+	-device virtio-gpu-gl -initrd initrd.img -smp 2,cores=2
 	$*
 
 #	-enable-kvm \
+#	-vga virtio -display gtk,zoom-to-fit=on,gl=on \
