d47a430698e03a2e8b57a25721af972536de40f7
Kai Horstmann <horstmannkai@hotmail.com>
2026-07-09 20:48:48 +0200

    startQemuDirect.sh: New. Start Qemu with the compiled kernel on the disk image sd.img

--- startQemuDirect.sh
+++ startQemuDirect.sh
@@ -4,8 +4,7 @@ qemu-system-x86_64 -kernel build/kernel/arch/x86/boot/bzImage -append "console=t
 	-drive file=sd.img,format=raw,if=ide \
 	-vga virtio -display gtk,gl=on \
 	-m 2G \
-	-device virtio-gpu-gl -initrd initrd.img -smp 2,cores=2
-	$*
+	-device virtio-gpu-gl -initrd initrd.img -smp 2,cores=2 $*
 
 #	-enable-kvm \
 #	-vga virtio -display gtk,zoom-to-fit=on,gl=on \
